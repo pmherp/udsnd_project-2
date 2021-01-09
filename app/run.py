76f2bf06d4dee7c94aa2b7db3de0b047ef175ea4
pmherp <pmherp.git@gmail.com>
2021-01-09 14:55:39 +0100

    feat: working app with two graphs

--- app/run.py
+++ app/run.py
@@ -8,7 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-import joblib
+import joblib #older version depricated
 from sqlalchemy import create_engine
 
 
